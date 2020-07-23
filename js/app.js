f95b98a5fb53c45b96778f6d0f26edaa03b9aa13
Nikita Belevich <belevish_1998@mail.ru>
2020-07-23 14:25:42 +0300

    Task 1 complete
    Задачка на случайные числа и задержку setTimeout

--- js/app.js
+++ js/app.js
@@ -10,8 +10,28 @@ function delay(ms) {
 
 }
 delay(3000).then(
-    result => {alert(result)}
+    // result => {alert(result)}
 );
 
+const task1 = document.querySelector('.task1');
 
-// console.log(promise);
+let promise2 = new Promise((resolve, reject) => {
+    let randomNumber = getRandomInt(1, 10);
+    setTimeout(() => {
+        if (randomNumber >= 1 && randomNumber <= 5) resolve(randomNumber);
+        if (randomNumber > 5) reject(new Error(`Случайное число ${randomNumber} > 5`))
+    }, randomNumber * 1000);
+})
+
+promise2.then(
+    result => {
+        task1.textContent = `Случайное число ${result}`;
+    },
+    error => {
+        task1.textContent = error;
+    }
+);
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
